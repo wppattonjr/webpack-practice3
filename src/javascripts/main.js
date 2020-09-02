b12f331a094be07c689827199e256f719431c062
Wendell <wendell.pattonjr@gmail.com>
2020-09-02 18:31:19 -0500

    practic 3 complete

--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -2,7 +2,7 @@ import '../styles/main.scss';
 
 const init = () => {
   $('#app').html('<h1>HELLO! You are up and running!</h1>');
-  console.log('YOU ARE UP AND RUNNING!');
+  console.error('YOU ARE UP AND RUNNING!');
 };
 
 init();
